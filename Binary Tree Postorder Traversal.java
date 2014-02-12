6c58b1d3e9dedacc3e544c961f3adbd0af48ed68
2014dream <stevencraglist@gmail.com>
2014-02-12 12:22:19 -0700

    Update Binary Tree Postorder Traversal.java

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -1,5 +1,7 @@
 /*Given a binary tree, return the postorder traversal of its nodes' values.*/
 
+/*The first method space complexity is O(h), where h is the height of the tree. 
+The second methof sapce complexity is O(n), where n is the number of nodes.*/
 /* This is the first intenative version using just one stack. In this method, there are two pointers, pre and current;
 when up  to bottom, the pre is the parent of current; when bottom to up, pre is the child of current.*/
 
@@ -38,6 +40,34 @@ public class Solution {
                 nodes.pop();
             }
             pre=current;
+        }
+        return result;
+    }
+}
+
+
+/* This is the second verion of intenative. There are two stacks. It is doing a reversed pre-order traversal. That is,
+the orider of traversal is a node, then the right child follewd by its left child. This yields the post-order in reversd
+order. Using a second stack, we could reverse it back.*/
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(null==root)
+            return result;
+        Stack<TreeNode> nodes= new Stack<TreeNode>();
+        Stack<TreeNode> output=new Stack<TreeNode>();
+        nodes.push(root);
+        while(false==nodes.empty()){
+            TreeNode current=nodes.peek();
+            nodes.pop();
+            output.push(current);
+            if(null!=current.left)
+                nodes.push(current.left);
+            if(null!=current.right)
+                nodes.push(current.right);
+        }
+        while(false==output.empty()){
+            result.add(output.pop().val);
         }
         return result;
     }
